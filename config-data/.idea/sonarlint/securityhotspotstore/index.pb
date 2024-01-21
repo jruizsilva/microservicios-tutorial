2eae4eab6faa3a16e5911fa74867fd09e75b5344
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-21 15:29:15 -0300

    eureka

--- config-data/.idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,7 +0,0 @@
-
-@
-car-service.yaml,a\5\a54f8b7c588d8fc0524243a2be593b1eea1f41ed
-A
-moto-service.yaml,5\6\56017bd8c3d6a648d0c1de24cbb32c947ecb0902
-A
-user-service.yaml,f\b\fbfedec96d99ff858262649f8708e8675b3ea3f2
--- eureka-service/src/main/java/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/eurekaservice/EurekaServiceApplication.java
@@ -1,13 +1,15 @@
-package carservice;
+package eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class CarServiceApplication {
+@EnableEurekaServer
+public class EurekaServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CarServiceApplication.class,
+        SpringApplication.run(EurekaServiceApplication.class,
                               args);
     }
 
