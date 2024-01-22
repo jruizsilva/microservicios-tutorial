d925db174113f12bead222d5455a3ee1480828b7
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-21 22:26:49 -0300

    circuit-breaker

--- user-service/src/main/java/userservice/controller/UserController.java
+++ user-service/src/main/java/userservice/controller/UserController.java
@@ -1,6 +1,8 @@
 package userservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import userservice.entities.User;
@@ -43,6 +45,8 @@ public class UserController {
         return ResponseEntity.ok(service.save(user));
     }
 
+    @CircuitBreaker(name = "cars-cb",
+                    fallbackMethod = "fallbackFindAllCarsByUserId")
     @GetMapping("/{userId}/cars")
     public ResponseEntity<List<Car>> findAllCarsByUserId(@PathVariable("userId") Long userId) {
         User user = service.findById(userId);
@@ -53,6 +57,8 @@ public class UserController {
         return ResponseEntity.ok(service.findAllCarsByUserId(userId));
     }
 
+    @CircuitBreaker(name = "motos-cb",
+                    fallbackMethod = "fallbackFindAllMotosByUserId")
     @GetMapping("/{userId}/motos")
     public ResponseEntity<List<Moto>> findAllMotosByUserId(@PathVariable("userId") Long userId) {
         User user = service.findById(userId);
@@ -63,6 +69,8 @@ public class UserController {
         return ResponseEntity.ok(service.findAllMotosByUserId(userId));
     }
 
+    @CircuitBreaker(name = "cars-cb",
+                    fallbackMethod = "fallbackSaveCar")
     @PostMapping("/{userId}/cars")
     public ResponseEntity<Car> saveCar(@PathVariable("userId") Long userId,
                                        @RequestBody Car car) {
@@ -70,6 +78,8 @@ public class UserController {
                                                  car));
     }
 
+    @CircuitBreaker(name = "motos-cb",
+                    fallbackMethod = "fallbackSaveMoto")
     @PostMapping("/{userId}/motos")
     public ResponseEntity<Moto> saveMoto(@PathVariable("userId") Long userId,
                                          @RequestBody Moto moto) {
@@ -77,8 +87,42 @@ public class UserController {
                                                   moto));
     }
 
+    @CircuitBreaker(name = "todos-cb",
+                    fallbackMethod = "fallbackGetUserAndVehicles")
     @GetMapping("{userId}/todos")
     public ResponseEntity<Map<String, Object>> getUserAndVehicles(@PathVariable("userId") Long userId) {
         return ResponseEntity.ok(service.getUserAndVehicles(userId));
+    }
+
+    private ResponseEntity<String> fallbackFindAllCarsByUserId(@PathVariable("userId") Long userId,
+                                                               RuntimeException exception) {
+        return new ResponseEntity<>("can't get user cars",
+                                    HttpStatus.OK);
+    }
+
+    private ResponseEntity<String> fallbackSaveCar(@PathVariable("userId") Long userId,
+                                                   @RequestBody Car car,
+                                                   RuntimeException exception) {
+        return new ResponseEntity<>("can't save car",
+                                    HttpStatus.OK);
+    }
+
+    private ResponseEntity<String> fallbackFindAllMotosByUserId(@PathVariable("userId") Long userId,
+                                                                RuntimeException exception) {
+        return new ResponseEntity<>("can't get user motos",
+                                    HttpStatus.OK);
+    }
+
+    private ResponseEntity<String> fallbackSaveMoto(@PathVariable("userId") Long userId,
+                                                    @RequestBody Car car,
+                                                    RuntimeException exception) {
+        return new ResponseEntity<>("can't save moto",
+                                    HttpStatus.OK);
+    }
+
+    private ResponseEntity<String> fallbackGetUserAndVehicles(@PathVariable("userId") Long userId,
+                                                              RuntimeException exception) {
+        return new ResponseEntity<>("can't get user vehicles",
+                                    HttpStatus.OK);
     }
 }
